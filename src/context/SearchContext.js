d299466d640ef08cd904a31202df87f22bb6bde4
Preston Darrough <techdarrough@gmail.com>
2022-04-10 10:52:00 -0700

    use ref being added

--- src/context/SearchContext.js
+++ src/context/SearchContext.js
@@ -2,5 +2,24 @@ import {createContext} from 'react'
 
 export const SearchContext = createContext({
     term: '',
-    handleSearch: () => {}
+    handleSearch: (search) => {
+        useEffect(() => {
+            if (search) {
+                const fetchData = async () => {
+                    document.title = `${search} Music`
+                    const response = await fetch(API_URL + search)
+                    const resData = await response.json()
+                    if(resData.results.length > 0) {
+                        return setData(resData.results)
+                    } else {
+                        return setMessage('Not Found.')
+                    }
+                }
+                fetchData()
+            }
+        }, [search])
+    }
 })
+
+
+
